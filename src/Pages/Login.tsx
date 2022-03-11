147d34a0b06c94d7be5ef047548135978b1902f8
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-10 23:10:01 -0300

    feat: create logic for show and hide login and sign up

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,13 +1,44 @@
+import { Input, Button, LoginText } from '../components';
+import { useState } from 'react';
+
 export const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
     <div
       className={
-        "bg-amber-300 w-screen h-screen flex items-center justify-center"
+        'bg-amber-300 w-screen h-screen flex items-center justify-center'
       }
     >
-      <div className={"bg-white rounded shadow-lg "}>
-        <form className={"p-6"}>
-          <h1 className={"font-bold text-lg"}>Faça seu Login</h1>
+      <div className={'bg-white rounded shadow-lg m-6'}>
+        <form className={'p-6'}>
+          {isLogin ? (
+            <LoginText
+              title={'Faça seu Login'}
+              text={'Você ainda não tem conta? então faça o'}
+              linkText={'cadastro'}
+              onClick={() => setIsLogin(false)}
+            />
+          ) : (
+            <LoginText
+              title={'Faça seu Cadastro'}
+              text={'Faça seu cadastro agora'}
+              linkText={'voltar'}
+              onClick={() => setIsLogin(true)}
+            />
+          )}
+
+          <div className={'flex flex-col mt-4 gap-1.5'}>
+            <Input type="email" placeholder={'E-mail'} />
+            <Input type="password" placeholder={'Senha'} />
+          </div>
+          <div className={'mt-2'}>
+            {isLogin ? (
+              <Button text={'Entrar'} onClick={() => {}} />
+            ) : (
+              <Button text={'Cadastrar'} onClick={() => {}} />
+            )}
+          </div>
         </form>
       </div>
     </div>
