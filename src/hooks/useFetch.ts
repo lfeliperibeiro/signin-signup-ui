bab844b58ba6375287784b5b95ede1089a4ed074
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-11 22:15:54 -0300

    feat: create hook for use api

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { signSignupUrl } from '../lib/axios';
+
+export function useFetch<T = unknown>(
+  url: string,
+  options?: AxiosRequestConfig
+) {
+  const [data, setData] = useState<T | null>(null);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    signSignupUrl
+      .post(url, options)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setIsFetching(false);
+      });
+  }, []);
+
+  return { data, error, isFetching };
+}
