9664c3934d21d94cd105cc17fa5a502c6d8fe483
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-11 22:10:55 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import "./index.css";
-import { Login } from "./Pages/Login";
+import './index.css';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './Pages/Login';
+import { Dashboard } from './Pages/DashBoard';
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path={'/'} element={<Login />} />
+      <Route path={'/dashboard'} element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;
