de7b02746462dbae93cc87198fd623252d3f7976
Felipe Ribeiro <piporibeiro@gmail.com>
2022-03-13 21:57:16 -0300

    chore: setup test

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+  ],
+  collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  moduleDirectories: ['node_modules', '.'],
+  resetMocks: true,
+  testEnvironment: 'jsdom',
+};
